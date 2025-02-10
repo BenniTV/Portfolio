866eaecedbadb82b179596d670392971225514bc
BenniTV <126875454+BenniTV@users.noreply.github.com>
2025-02-10 22:15:40 +0000

    test

---
+++ script.js
@@ -0,0 +1,14 @@
+const projects = [
+    { name: "Vokabeltrainer (SwiftUI)", link: "#" },
+    { name: "HamburgRP Website", link: "#hamburgrp.rf.gd" },
+    { name: "Feuerwehr-Info App", link: "#" }
+];
+
+const projectList = document.getElementById("project-list");
+
+projects.forEach(project => {
+    let div = document.createElement("div");
+    div.className = "project";
+    div.innerHTML = `<h3>${project.name}</h3><a href="${project.link}" target="_blank">Mehr erfahren</a>`;
+    projectList.appendChild(div);
+});
