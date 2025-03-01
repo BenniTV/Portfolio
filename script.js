2387e0ef8715796a819d8b811e0a4fba70b78697
BenniTV <126875454+BenniTV@users.noreply.github.com>
2025-03-01 21:20:55 +0100

    style

---
+++ script.js
@@ -0,0 +1,9 @@
+// DOM Elemente
+document.addEventListener('DOMContentLoaded', () => {
+    // Hier kommt später die Hauptlogik
+});
+
+// Hilfsfunktionen
+function animate(element) {
+    element.classList.add('animate-fade-in');
+} 
